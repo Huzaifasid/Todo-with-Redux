2dc7ba718138d4c330922fe5112246c6fba4d233
Huzaifasid <siddiquihuzaifa68@gmail.com>
2023-10-10 19:52:28 +0500

    update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
 
-
-ReactDom.render(<>
-<Provider store={store}>
-    <App />
-
-</Provider>
-</>
-    , document.getElementById("root"))
+ReactDom.render(
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>,
+  document.getElementById("root")
+);
